6e70be8fda38c255f240a4328f8a14a0c4daaea3
binhqnguyen <binhnguyenns3@gmail.com>
2013-07-03 12:01:07 -0600

    -Modified automated scripts

--- rp-emulated-nsc.sh
+++ rp-emulated-nsc.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-E_NAME='CUBIC_20'
-
+E_NAME='CUBIC_noname'
+PY_DIR='dir_of_python_scripts'
+RE_DIR='/var/tmp/ln_result/'
+if [ "$1" != "" && "$2" != "" ];
+	then
+	E_NAME=$1
+	PY_DIR=$2
+	else
+		echo "usage rp-...sh <name of backup folder> <where are python scripts>"
+		exit;
+fi
+echo $E_NAME 
+echo $PY_DIR
+echo $RE_DIR
 
 echo "running emulated nsc UDP ..."
 ./waf --run "scratch/emulated_nsc --sim_time=100 --is_tcp=0" > /var/tmp/ln_result/emulated/UDP_LOG 2>&1 
@@ -8,10 +20,10 @@ echo "running emulated nsc UDP ..."
 echo "running emulated nsc TCP ..."
 ./waf --run "scratch/emulated_nsc --sim_time=100 --is_tcp=1" > /var/tmp/ln_result/emulated/TCP_LOG 2>&1 
 
-mkdir /var/tmp/ln_result/backup/emulated/$E_NAME
-cp -rf /var/tmp/ln_result/emulated/ /var/tmp/ln_result/backup/emulated/$E_NAME 
+mkdir $RE_DIR\backup/emulated/$E_NAME
+cp -rf $RE_DIR\emulated/ $RE_DIR\backup/emulated/$E_NAME 
 
-cd /var/tmp/ln_result/
-./dr-emulated-nsc.sh $E_NAME
+cd $RE_DIR
+./dr-emulated-nsc.sh $E_NAME $PY_DIR
 
 
--- rp-radio-nsc.sh
+++ rp-radio-nsc.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-E_NAME=CUBIC_20
+E_NAME='CUBIC_noname'
+PY_DIR='/var/tmp/ln_result/radio/'
+RE_DIR='/var/tmp/ln_result/'
+if [ "$1" != "" ];
+	then
+	E_NAME=$1
+	else
+		echo "usage rp-..sh <name of backup folder>"
+		exit;
+fi
+echo $E_NAME 
+echo $PY_DIR
+echo $RE_DIR
+
 
 echo "running lte nsc UDP ..."
 ./waf --run "scratch/lte_nsc --simTime=100 --isTcp=0" > /var/tmp/ln_result/radio/UDP_LOG 2>&1 
@@ -9,6 +22,9 @@ echo "running lte nsc TCP ..."
 
 mkdir /var/tmp/ln_result/backup/radio/$E_NAME
 cp -rf /var/tmp/ln_result/radio/ /var/tmp/ln_result/backup/radio/$E_NAME 
+
+cd $RE_DIR
+./dr-radio-nsc.sh $E_NAME $PY_DIR
 
 
 
